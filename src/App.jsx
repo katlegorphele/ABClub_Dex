b77beaa486b3a163890721a1e73571b6291fee4f
katlegorphele <katlegophele95@gmail.com>
2024-06-08 22:12:24 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+const App = () => {
+  return (
+    <div className="w-full justify-center">
+      <h1 className="flex items-center font-bold">My React App</h1>
+      <p>Welcome to my React App</p>
+    </div>
+  );
+}
+
+export default App;
